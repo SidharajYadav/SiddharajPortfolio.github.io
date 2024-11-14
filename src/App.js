132ecef876223d78e1c54e3650be0e9ed7cd9f31
SiddharajYadav <sidharajyadav157@gmail.com>
2024-11-14 09:30:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect, Suspense } from "react";
 import { darkTheme, lightTheme } from "./utils/Themes.js";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar"; 
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
